597e26c5453f3d6dca54cf26db6b2001669433c0
kingsleyonyelo <sabistore12@gmail.com>
2023-05-18 13:30:46 +0100

    fixed dropdown

--- components/Cardtwo.tsx
+++ components/Cardtwo.tsx
@@ -26,18 +26,18 @@ const Cardtwo = () => {
           </p>
         </div>
       </div>
-      <div className="sm:w-[48%] w-[100%] h-[90%] bg-white rounded-lg p-4 pr-0">
-        <div className="w-full flex bg-black pb-2">
-          <div className="w-[100%]">
-            <p className="sm:text-[22px] leading-snug break-normal font-gilroy mt-4 text-white">
-              Welcome to the world&apos;s most popular website builder
-            </p>
-
+      <div className="sm:w-[48%] w-[100%] h-[100%]">
+        <div className="w-[100%] flex-col  bg-black p-1 rounded-lg">
+          <p className="sm:text-2xl text-center leading-snug break-normal font-gilroy mt-5 text-white text-opacity-50">
+            Welcome to the world&apos;s most popular website builder
+          </p>
+          <span className="flex justify-center mt-4">
             <button className="p-2 bg-[#3373BE] text-white">Get started</button>
-            <div className="w-full h-[1px] bg-white mt-[60px]" />
-          </div>
+          </span>
+
+          <div className="w-full h-[1px] bg-white mt-[60px]" />
         </div>
-        <div className="w-full flex justify-center items-center mt-4 space-x-2">
+        <div className="w-full flex justify-center items-center mt-0 space-x-2 bg-white p-3">
           <Image src={django} alt="wordpress" />
           <p className="sm:text-[24px] font-semibold leading-snug break-normal font-gilroy">
             django
