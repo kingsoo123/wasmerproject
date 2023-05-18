0db910dfab1ec62e9c8be7838583aef303bbaeb2
kingsleyonyelo <sabistore12@gmail.com>
2023-05-18 09:39:25 +0100

    fixed dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,7 +67,7 @@ const Header = () => {
         <div
           className={`${
             !showdropdown && "hidden"
-          }   w-[400px] border rounded-lg absolute top-14 ${position} z-30 border border-[1px] bg-white transition-all duration-500 transform`}
+          }   w-[400px] border rounded-lg absolute top-14 ${position} z-30 border border-[1px] bg-white transition-all duration-200 transform`}
           onMouseEnter={() => setShowdropdown(true)}
           onMouseLeave={() => setShowdropdown(false)}
         >
