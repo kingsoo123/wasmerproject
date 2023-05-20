955b4f8b8e2a05b748dbf9ab7f87a2aef1d25498
kingsleyonyelo <sabistore12@gmail.com>
2023-05-20 09:43:42 +0100

    fixed dropdown

--- components/About.tsx
+++ components/About.tsx
@@ -196,13 +196,13 @@ const About = () => {
         </div>
         <div className=" w-full flex justify-center items-center mt-[100px]">
           <div className="sm:w-[90%] w-full relative">
-            <div className="w-full md:flex flex-row justify-center items-center space-x-4">
+            <div className="w-full md:flex flex-row justify-center items-center sm:space-x-4 sm:pl-0 pl-4">
               {/* insert cards */}
               {[1, 2, 3].map((item, id) => (
                 <PackageCard key={id} />
               ))}
             </div>
-            <div className="w-full flex justify-center items-center mt-8 ">
+            <div className="w-full flex justify-center items-center mt-8 sm:pl-o pl-4">
               <span className="sm:w-[50%] w-full sm:h-[118px] bg-[#0F0518] rounded-lg md:flex flex-row justify-between items-center p-5 orange">
                 <p className="text-[22px] text-white">
                   All languages, fully containerize {"&"} collaborative
